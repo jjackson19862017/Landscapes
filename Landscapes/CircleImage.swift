45e6a71c42ec34f5046c6c2c8cf116a572c274ee
Stephen Jackson <jjackson19862017@gmail.com>
2020-05-06 12:11:46 +0100

    End of Lesson 1

---
+++ Landscapes/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Landscapes
+//
+//  Created by Stephen Jackson on 06/05/2020.
+//  Copyright © 2020 Stephen Jackson. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay(
+                Circle().stroke(Color.white, lineWidth: 4))
+        .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
